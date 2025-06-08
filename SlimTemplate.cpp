f9e0f1577b420d5f4f74db49b5c036a4f80aed0f
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2025-06-08 21:05:48 +0600

    Add initial implementation of solution template and input/output files

---
+++ SlimTemplate.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+};
+
+int main() {
+  ifstream fin("input.txt");
+  ofstream fout("output.txt");
+  int testCases = 1;
+// check if LOCAL is defined
+#ifdef LOCAL
+  fin >> testCases;
+#endif
+  Solution s;
+  while (testCases--) {
+    // take input here
+
+    // write your code here
+  }
+  fin.close();
+  fout.close();
+  return 0;
+}
