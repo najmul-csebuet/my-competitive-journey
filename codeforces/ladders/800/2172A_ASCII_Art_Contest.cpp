2c31d348b60ffc1c5dd2fc2aa965a6fad0adbb96
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2025-11-22 12:59:25 +0600

    WIP

---
+++ codeforces/ladders/800/2172A_ASCII_Art_Contest.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+
+void solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+#ifdef LOCAL
+  freopen("in.txt", "r", stdin);
+#endif
+
+  int testCases;
+  cin >> testCases;
+
+  while (testCases--) {
+    // take input here
+
+    // call solve function
+    solve();
+  }
+  return 0;
+}
