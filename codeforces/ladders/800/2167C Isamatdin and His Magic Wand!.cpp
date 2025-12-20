b1c220d12b45d4b0f84dfe7cd8ba4f1c249e8fdb
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2025-12-20 11:48:12 +0600

    AC: 2167C - Isamatdin and His Magic Wand!

--- codeforces/ladders/800/2167C Isamatdin and His Magic Wand!.cpp
+++ codeforces/ladders/800/2167C Isamatdin and His Magic Wand!.cpp
@@ -3,39 +3,23 @@ using namespace std;
 
 using vi = vector<int>;
 
+// AC
 void solve(vi& a) {
-  int firstEmptyIndex = 0;
-  while (true) {
-    if (firstEmptyIndex == a.size()) {
-      // no place left to swap
-      break;
+  int oddCount = 0;
+  for (int i = 0; i < a.size(); ++i) {
+    if (a[i] % 2 == 1) {
+      oddCount++;
     }
-    // find min element index and check if we can place it at the first position
-    int minIndex = firstEmptyIndex;
-    for (int i = firstEmptyIndex + 1; i < a.size(); ++i) {
-      if (a[i] < a[minIndex]) {
-        minIndex = i;
-      }
-    }
-
-    if (a[firstEmptyIndex] <= a[minIndex]) {
-      // they are already lexicographically sorted
-      firstEmptyIndex++;
-      continue;
-    }
-
-    if (a[firstEmptyIndex] % 2 == a[minIndex] % 2) {
-      // parity same so no swap possible
-      firstEmptyIndex++;
-      continue;
-    }
-
-    swap(a[firstEmptyIndex], a[minIndex]);
-    firstEmptyIndex++;
   }
 
-  for (int i = 0; i < a.size(); ++i) {
-    cout << a[i] << " ";
+  if (oddCount > 0 and oddCount < a.size()) {
+    sort(a.begin(), a.end());
+  }
+
+  // print sorted array
+  cout << a[0];
+  for (int i = 1; i < a.size(); ++i) {
+    cout << " " << a[i];
   }
   cout << endl;
 }
